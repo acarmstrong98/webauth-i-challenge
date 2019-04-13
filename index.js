b51b8132bc22054f90415e0006b2bf2bdb0f35c2
Andrew <acarmstrong98@gmail.com>
2019-04-13 12:59:23 -0400

    MVP complete

--- index.js
+++ index.js
@@ -53,5 +53,47 @@ server.post('/api/login', (req, res)=>{
 })
 })
 
+//Middleware 
+
+function restricted(req, res, next){ 
+  const {username, password } = req.headers;
+  if (username && password) {
+      Users.findBy({username})
+      .first()
+      .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)){
+          next();
+
+        }
+        
+        else{
+      res.status(401).json({message:'You shall not pass'})
+
+        }
+
+      })
+      .catch(err =>{
+        res.status(500).json(err);
+      })
+
+     }
+     
+     else{
+  res.status(400).json({message:'You shall not pass'})
+      }
+  };
+
+  server.get('/api/users', restricted, (req, res) => {
+
+    Users
+      .find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err))
+  });
+
+
+
 const port = process.env.PORT || 8080;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`)); 
